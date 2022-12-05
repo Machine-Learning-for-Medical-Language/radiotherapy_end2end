304fdbd886ff35b8db1fe7ffcbc52ac160e6f0af
Eli Goldner <egoldner@brandeis.edu>
2022-12-05 10:34:59 -0500

    HemOnc Results Reproduction Resources Added

---
+++ Training_Scripts/NER/fxfreq.sh
@@ -0,0 +1,34 @@
+task=rt_fxfreq
+dir="$(pwd)/HemOnc_RT_Data/NER/FxFreq/"
+epochs=20
+lr=2e-5
+ev_per_ep=2
+stl=0
+seed=42
+gas=4
+lr_type=cosine
+encoder=roberta-base
+dmy=$(date +'%m_%d_%Y')
+logging_dir="/home/$USER/step_recovery/logs/$task/$encoder/ep=$epochs-lr=$lr-stl=$stl-seed=$seed-gas=$gas/"
+mkdir -p $logging_dir
+logging_file="$logging_dir/$dmy"
+touch $logging_file
+temp="/home/$USER/step_recovery/$task/$encoder/ep=$epochs-lr=$lr-stl=$stl-seed=$seed-gas=$gas/$dmy/"
+cache="/home/$USER/step_recovery/caches/$task/$encoder/ep=$epochs-lr=$lr-stl=$stl-seed=$seed-gas=$gas/$dmy"
+mkdir -p $cache
+mkdir -p $temp
+python -m cnlpt.train_system \
+--task_name $task \
+--data_dir $dir \
+--encoder_name $encoder \
+--do_train \
+--cache $cache \
+--output_dir $temp \
+--overwrite_output_dir \
+--evals_per_epoch $ev_per_ep \
+--do_eval \
+--num_train_epochs $epochs \
+--learning_rate $lr \
+--lr_scheduler_type $lr_type \
+--seed $seed \
+--gradient_accumulation_steps $gas #>>$logging_file 2>&1
---
+++ Training_Scripts/NER/fxno.sh
@@ -0,0 +1,35 @@
+task=rt_fxno
+dir="$(pwd)/HemOnc_RT_Data/NER/FxNo/"
+epochs=15
+lr=1e-5
+ev_per_ep=2
+stl=0
+seed=42
+gas=4
+lr_type=constant
+encoder="emilyalsentzer/Bio_ClinicalBERT"
+dmy=$(date +'%m_%d_%Y')
+logging_dir="/home/$USER/step_recovery/logs/$task/$encoder/ep=$epochs-lr=$lr-stl=$stl-seed=$seed-gas=$gas/"
+mkdir -p $logging_dir
+logging_file="$logging_dir/$dmy"
+touch $logging_file
+temp="/home/$USER/step_recovery/$task/$encoder/ep=$epochs-lr=$lr-stl=$stl-seed=$seed-gas=$gas/$dmy/"
+cache="/home/$USER/step_recovery/caches/$task/$encoder/ep=$epochs-lr=$lr-stl=$stl-seed=$seed-gas=$gas/$dmy"
+mkdir -p $cache
+mkdir -p $temp
+python -m cnlpt.train_system \
+--task_name $task \
+--data_dir $dir \
+--encoder_name $encoder \
+--do_train \
+--cache $cache \
+--output_dir $temp \
+--overwrite_output_dir \
+--evals_per_epoch $ev_per_ep \
+--do_eval \
+--num_train_epochs $epochs \
+--learning_rate $lr \
+--lr_scheduler_type $lr_type \
+--seed $seed \
+--save_total_limit $stl \
+--gradient_accumulation_steps $gas #>>$logging_file 2>&1
